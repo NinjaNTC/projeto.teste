ccb510daced2cc12ad1b37f5cd86f550912331da
Eduardo <eduardo.ac2003@aluno.ifsc.edu.br>
2023-07-24 19:34:53 -0300

    first commit

---
+++ .vscode/scripts.js
@@ -0,0 +1,26 @@
+function toggleMode () {
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    const img = document.querySelector("#profile img")
+
+    if(html.classList.contains("light")) {
+        // O que acontece se a classlist tiver "light"
+    img.setAttribute("src","./assets/Avatar-light.png")
+    } else{
+    img.setAttribute("src","./assets/Avatar.png")
+    }
+  
+    const alt = document.querySelector("#profile img")
+
+    if(html.classList.contains("light")) {
+
+    img.setAttribute("alt", "Foto do modo claro de Maykebrito")
+    }
+    else{
+    img.setAttribute("alt", "Foto padrão do Maykebrito")
+    }
+
+
+
+}
